d0192eea8656875eca0def9ff4e5fc86a7bc63fe
Thomas Szymanski <thomas.szymanski15@gmail.com>
2017-10-03 10:20:20 +0200

    Correction exercice 4 prairie PHP

--- index.php
+++ index.php
@@ -1,33 +1,20 @@
 <?php
-
 $Fizz = 'Fizz ';
 $Buzz = 'Buzz ';
 $FizzBuzz = 'FizzBuzz ';
-
-
-    for ($nombre=0; $nombre<=100; $nombre++){
-
-
+  for ($nombre=0; $nombre<=100; $nombre++){
       switch ($nombre)
       {
           case $nombre % 3 == 0:
               echo $Fizz;
           break;
-
           case $nombre % 5 == 0:
               echo $Buzz;
           break;
-
           case $nombre % 3 == 0 AND $nombre % 5 == 0:
               echo $FizzBuzz;
           break;
-
-          default:
-              echo $nombre.' ';
+          default: echo $nombre.' ';
       }
-
 }
-
-
-
       ?>
